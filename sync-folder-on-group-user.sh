f06b7c5f1e963452696a9be3f996fa40f1df476f
picupup <44735171+picupup@users.noreply.github.com>
2025-02-08 16:58:08 +0100

    Update sync-folder-on-group-user.sh

--- sync-folder-on-group-user.sh
+++ sync-folder-on-group-user.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+# In this script it syncs the browser page with the content from the repository.
+# Variable `$LOGNAME` is used instead of `$HOME` or `$USER`, because they might not be set while cronjob is running
 
 echo -e "\n\n\n[$(date '+%FT%T')]\n\n"
 set -e  # Exit on error
 set -x  # Debug mode
 
 groupuser=${1:-"cia-2024"}
+name="bericht_webseite"
+repo="/home/$LOGNAME/repos/$name"
+dest_dir="/var/www/html/${groupuser}"
 
-repo=${2:-"/home/$LOGNAME/repos/bericht_webseite"}
-dest_dir=${3:-"/var/www/html/${groupuser}"}
-
-copy=$(mktemp -d)
+copy=/home/$LOGNAME/tmp/$name
+mkdir -p $copy
 
 # Ensure repo exists
 if [[ ! -d "$repo" ]]; then
@@ -30,6 +33,4 @@ find "$copy" -type d -exec chmod 755 {} + \
 # Transfer to target user
 sudo -iu ${groupuser} bash -c "rsync -av '$copy/' '$dest_dir/'"
 
-# Cleanup
-rm -rf "$copy"
 echo "Deployment successful!"
